494a3b4c2e278f55e20915bd06f7aa30cc8289ac
Christopher Bohl & Jdeesy <pair+cbohl.jdeesy@devbootcamp.com>
2015-04-17 15:08:49 -0500

    worked on layout and routes

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -6,6 +6,15 @@ get '/users' do
   end
 end
 
+post '/' do
+  user = User.find_by(email: params[:email])
+
+  # if user && user.password_hash = params[:password]
+  # session[:user_id] =
+  # unless session[:user_id]
+  #   redirect '/'
+end
+
 get '/users/new' do
   unless session[:user_id]
     redirect '/'
