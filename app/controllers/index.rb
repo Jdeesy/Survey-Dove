cf9cfa2b47ca3df303904a7ae28b87966abc36c1
John Dees <jdees08@gmail.com>
2015-04-17 22:04:15 -0500

    testing a solution to get the suvery creation to work

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,3 +7,32 @@ get '/' do
   end
 end
 
+get '/choices' do
+  @choices = Choice.all
+  erb :"choices/index", locals: {choices: @choices}
+end
+
+post '/choices' do
+  puts params
+  choice = Choice.create!(params)
+  if request.xhr?
+    erb :"choices/show", locals: {choice: choice}, layout: false
+  else
+    redirect "/choices"
+  end
+end
+
+get "/questions" do
+  @questions = Question.all
+  erb :"questions/index", locals: {questions: @questions}
+end
+
+post '/questions' do
+  puts params
+  question = Question.create!(params)
+  if request.xhr?
+    erb :"questions/show", locals: {question: question}, layout: false
+  else
+    redirect "/questions"
+  end
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,25 +1,36 @@
 $(document).ready(function(){
-  // $("form[name = add_choice]").submit(function(event){
+  $("form[name = add_choice]").submit(function(event){
+    event.preventDefault();
 
-  //   event.preventDefault();
-  //   console.log(event)  
+    addChoice = $.ajax({
+      url: $(this).attr("action"),
+      type: "post",
+      data: $(this).serialize()
+    });
 
-  //   addChoice = $.ajax({
-  //     url: $(this).attr("action"),
-  //     type: "post",
-  //     data: $(this).serialize
-  //   });
+    addChoice.done(function(response){
+      console.log("Done"),
+      $(".choices").append(response)
+      $("input[name = name]").val("")
+    });
+  });
 
-  //   addChoice.done(function(response){
-  //     $(".choices").append(response)
+  $("form[name = add_question]").submit(function(event){
+      event.preventDefault();
+console.log($(this).serialize()),
+      addQuestion = $.ajax({
+        
+        url: $(this).attr("action"),
+        type: "post",
+        data: $(this).serialize()
+      });
 
-  //   });
-
-
-  // });
-
-
-
+      addQuestion.done(function(response){
+        console.log("Done"),
+        $(".questions").append(response)
+        $("input[name = name]").val("")
+      });
+    });
 
 
 });
