322be0888a890445092c89b67728e7388eae332e
John Dees <jdees08@gmail.com>
2015-04-18 12:43:38 -0500

    working on mvp solution

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,51 +7,51 @@ get '/' do
   end
 end
 
-get '/choices' do
-  @choices = Choice.all
-  erb :"choices/index", locals: {choices: @choices}
-end
+# get '/choices' do
+#   @choices = Choice.all
+#   erb :"choices/index", locals: {choices: @choices}
+# end
 
-post '/choices' do
-  puts params
-  choice = Choice.create!(params)
-  if request.xhr?
-    erb :"choices/show", locals: {choice: choice}, layout: false
-  else
-    redirect "/choices"
-  end
-end
+# post '/choices' do
+#   puts params
+#   choice = Choice.create!(params)
+#   if request.xhr?
+#     erb :"choices/show", locals: {choice: choice}, layout: false
+#   else
+#     redirect "/choices"
+#   end
+# end
 
-get "/questions" do
-  @questions = Question.all
-  erb :"questions/index", locals: {questions: @questions}
-end
+# get "/questions" do
+#   @questions = Question.all
+#   erb :"questions/index", locals: {questions: @questions}
+# end
 
-post '/questions' do
-  puts params
+# post '/questions' do
+#   puts params
 
-  if params[:name]
-    question = Question.find(1)
-    choices =  question.choices
-    choice = Choice.create!(params)
-    choices << choice
+#   if params[:name]
+#     question = Question.find(1)
+#     choices =  question.choices
+#     choice = Choice.create!(params)
+#     choices << choice
 
-    if request.xhr?
-      erb :"choices/show", locals: {choice: choice}, layout: false
-    else
-      redirect "/questions"
-    end
+#     if request.xhr?
+#       erb :"choices/show", locals: {choice: choice}, layout: false
+#     else
+#       redirect "/questions"
+#     end
 
-  else
-    question = Question.create!(params)
-    choices = question.choices
+#   else
+#     question = Question.create!(params)
+#     choices = question.choices
 
-    if request.xhr?
-      erb :"questions/show", locals: {question: question, choices: choices}, layout: false
-    else
-      redirect "/questions"
-    end
-  end
+  #   if request.xhr?
+  #     erb :"questions/show", locals: {question: question, choices: choices}, layout: false
+  #   else
+  #     redirect "/questions"
+  #   end
+  # end
 
 
-end
+# end
--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -3,39 +3,42 @@
 #index
 get '/surveys' do
   #display all surveys
-  if session[:user_id]
+  # if session[:user_id]
+    @surveys = Survey.all
     erb :'surveys/index'
-  else
-    redirect '/'
-  end
-
-end
-
-#new
-get '/surveys/new' do
-  unless session[:user_id]
-    redirect '/'
-  else
-
-  end
+  # else
+  #   redirect '/'
+  # end
 
 end
 
 #create
 post '/surveys' do
-
+  puts params
+  @survey = Survey.create!(name: params[:name])
+  redirect "/surveys/#{@survey.id}/new"
 end
+
+#new
+get '/surveys/:id/new' do
+  @questions = Survey.find(params[:id]).questions
+  erb :"questions/index"
+end
+
 
 #show
 get '/surveys/:id' do
-  unless session[:user_id]
-    redirect '/'
-  else
+    @survey = Survey.find(params[:id])
     erb :'surveys/show'
-  end
-
-  #if user has taken survey, show the results
-
-  #else have them take the survey
 end
 
+post '/surveys/:id/new' do
+  puts params
+  if request.xhr?
+    question = Question.create(prompt: params[:prompt], survey_id: params[:id])
+    choices = question.choices
+    erb :"questions/show", locals: {question: question, choices: choices}, layout: false
+  else
+    redirect "/questions"
+  end
+end
