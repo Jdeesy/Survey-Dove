97df1574d297aaad7f60ebad647289f68b591e7c
John Dees <jdees08@gmail.com>
2015-04-17 20:28:41 -0500

    js file

---
+++ public/js/application.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  // $("form[name = add_choice]").submit(function(event){
+
+  //   event.preventDefault();
+  //   console.log(event)  
+
+  //   addChoice = $.ajax({
+  //     url: $(this).attr("action"),
+  //     type: "post",
+  //     data: $(this).serialize
+  //   });
+
+  //   addChoice.done(function(response){
+  //     $(".choices").append(response)
+
+  //   });
+
+
+  // });
+
+
+
+
+
+});
